27074e5edf8ed4afc3d962b5d8c1b92d7c4c2e2e
Andrej <xdrej92@gmail.com>
2023-04-16 21:05:16 +0200

    almost done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -33,7 +33,7 @@ if($('.professionals-slider').length > 0){
     });
 }
 
-/*
+
 
     if ($('.contact-form').length > 0) {
         $(function () {
@@ -47,34 +47,19 @@ if($('.professionals-slider').length > 0){
                     $(element).closest('.form-group').addClass("is-valid").removeClass("is-invalid");
                 },
                 rules: {
-                    name: {
+                    contact_name: {
                         required: true
                     },
-                    email: {
+                    contact_email: {
                         required: true,
                         email: true
                     },
-                    message: {
-                        required: true,
-                        maxlength: 255
+                    contact_message: {
+                        required: true
 
                     }
                 },
-                messages: {
-                    name: {
-                        required: 'Name* is required field!!!'
-                    },
-                    email: {
-                        required: 'Email* is required field!!!',
-                        email: 'Please insert valid Email address!!!'
-                    },
-                    message: {
-                        required: 'Message* is required field!!!',
-                        maxlength: 'Max Message length is 255 characters !!!'
-
-                    }
-
-                },
+               
                 errorElement: 'p',
                 errorPlacement: function (error, element) {
                     error.appendTo($(element).closest('.form-group').find('.invalid-feedback'));
@@ -84,6 +69,5 @@ if($('.professionals-slider').length > 0){
         });
     }//Form Validation
 
-*/
 
 });
